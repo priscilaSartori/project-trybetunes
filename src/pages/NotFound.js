b77318f5e8388ee8f443432dfeb8b94c2d753887
Priscila <priscilasartori2@gmail.com>
2022-10-04 18:45:13 -0300

    Requisito 5

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 class NotFound extends React.Component {
   render() {
     return (
-      <div data-testid="page-not-found"></div>
+      <div data-testid="page-not-found" />
     );
   }
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,56 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    login: '',
+    isSaveButtonDisabled: true,
+  };
+
+  buttonSave = () => {
+    const { login } = this.state;
+    const maxNumber = 1;
+    if (login.length > maxNumber) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  };
+
+  onInputChange = (event) => {
+    const { value } = event.target;
+    this.setState({ login: value }, () => { this.buttonSave(); });
+  };
+
   render() {
+    const {
+      login,
+      isSaveButtonDisabled,
+    } = this.state;
     return (
-      <div>
+      <div data-testid="page-search">
         <Header />
-        <div data-testid="page-search" />
+        <form>
+          <label htmlFor="name">
+            Nome
+            <input
+              data-testid="search-artist-input"
+              id="name"
+              name="login"
+              type="text"
+              value={ login }
+              onChange={ this.onInputChange }
+              placeholder="Digite o nome da banda ou do artista"
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ isSaveButtonDisabled }
+            onClick={ this.onButtonClick }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
